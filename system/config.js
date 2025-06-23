ca525864c67a4047b4fd57bb12d6487658865f9e
uzairqari <uzairstories80@gmail.com>
2025-06-23 20:49:13 +0500

    Add files via upload

---
+++ system/config.js
@@ -0,0 +1,21 @@
+/*
+┏━━━━━━━━━━━━━━━┓  
+┃ dxyro BASE - WHATSAPP     
+┣━━━━━━━━━━━━━━━┛
+┃♕ Creator: dxyro           
+┃♕ AI Helper: ChatGPT             
+┃♔ Version: 1.0.0                   
+┗━━━━━━━━━━━━━━━┛
+*/
+//========RIKZZ========
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; 
+global.publik = true
+global.owner = ["62858931405584"] 
+global.namabot = '—͟͞͞𝐕𝐚𝐭𝐨𝐧𝐢𝐜 - 𝐗 𝐂𝐫𝐚𝐬𝐡𝐞𝐫'
+//======================
+global.mess = { 
+owner: 'waduhh!, lu bukan owner gw bg',
+premium: 'anda bukan user premium',
+succes: 'done bang'
+}
+//======================
